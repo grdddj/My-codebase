91282ff1e2c4283612ea060d69690f0bd221b8d4
Jiri Musil <jiri.musil06@seznam.cz>
2019-02-23 17:09:05 +0100

    Adding email sending and CSV export

--- Python Sending emails/mailAndCsv.py
+++ Python Sending emails/mailAndCsv.py
@@ -1,11 +1,14 @@
+import smtplib, ssl
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
+from datetime import datetime
 
+# Defining the array of players whose ELO to fetch
 eloNumbers = [339300, 306576, 345652]
 
-info = []
-
+# Defining date, to include it into name of CSV file, to avoid overwriting
+date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 # Array to store all the player's name and ratings
 playersInfo = []
@@ -35,3 +38,34 @@ for player in playersInfo:
 		playersInfoToPrint = playersInfoToPrint + "\n"
 	
 print(playersInfoToPrint)
+
+# Saving the output as an CSV file
+with open("elo-" + date + ".csv", "w") as csv_file:
+	csv_writer = writer(csv_file)
+	headers = ["Name", "Classic", "Rapid", "Blitz"]
+	csv_writer.writerow(headers)
+
+	for player in playersInfo:
+		playerRow = []
+		for info in player:
+			playerRow.append(info)
+		csv_writer.writerow(playerRow)
+
+# Sending the output as an email
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "scraper006@gmail.com"  # Enter your address
+receiver_email = "jiri.musil006@gmail.com"  # Enter receiver address
+password = "+Scraper006+"
+
+message = """\
+Subject: Elo of chess players
+
+{playersInfo}
+""".format(playersInfo=playersInfoToPrint)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)

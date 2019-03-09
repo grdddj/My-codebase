9d5224b6862d77a1839e55091e8a6700cd59bc9b
Jiri Musil <jiri.musil06@seznam.cz>
2019-03-09 17:35:38 +0100

    US_presidents_signatures.py created

---
+++ Python/Wikipedia Scraping/README.md
@@ -0,0 +1,2 @@
+# Wikipedia scraping
+This folder is focused on automatic data retrieving from Wikipedia or other sources
---
+++ Python/Wikipedia Scraping/US_presidents_signatures.py
@@ -0,0 +1,52 @@
+"""
+This script is fetching the signatures of all presidents of USA
+and saving them locally in the form of .png files
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+# Initial page, from which we get the list of presidents
+page = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States"
+
+response = requests.get(page)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extracting the list of all the presidents
+all_presidents = soup.find(class_="wikitable").findAll("b")
+
+# Loop through all the president and gather data about him
+for president in all_presidents:
+    try:
+        name = president.find("a").get_text().strip()
+        link = "https://en.wikipedia.org" + president.find("a")["href"]
+
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        info = soup.find(class_="infobox vcard").findAll("tr")
+    except Exception as e:
+        print("Impossible to locate info-card: " + name)
+        continue
+
+    signature_link = ""
+
+    # Looking for the "Signature" field and retrieving the link to the image
+    for row in info:
+        try:
+            if row.find("th").get_text().strip() == "Signature":
+                signature_link = "https:" + row.find("img")["src"]
+                break
+        except Exception as e:
+            continue
+
+    # Increasing the quality of the picture
+    signature_link = signature_link.replace("128px", "256px")
+
+    # Saving the picture locally
+    try:
+        path = "D:\My-codebase\Python\Wikipedia Scraping\Signatures\{}.png".format(name)
+        urllib.request.urlretrieve(signature_link, path)
+    except Exception as e:
+        print(e)

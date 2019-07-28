cf62b16467c7c6cc288b66b8d70e17f27546f6d8
Jiri Musil <jiri.musil06@seznam.cz>
2019-07-28 20:39:35 +0200

    Adding Flask API template

---
+++ Python/API/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        return "Employees page"
+
+
+class Tracks(Resource):
+    def get(self):
+        return "Tracks page"
+
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        return "Employees_Name " + employee_id
+
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port='5002')

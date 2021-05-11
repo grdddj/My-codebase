368e442654163a5de5e19074a04b6518e09b1710
grdddj <jiri.musil06@seznam.cz>
2021-05-11 21:54:54 +0200

    Smaller improvements and fixes in Trubka calculator

--- JavaScript/Trubka calculator/trubka.js
+++ JavaScript/Trubka calculator/trubka.js
@@ -1,7 +1,11 @@
+let vysledne_kombinace = [];
+
 function spocitej_vysledky() {
   const delka_polotovaru = parseInt($("#delka_polotovaru").val());
   const delka_odpadu = parseInt($("#delka_odpadu").val());
-  const delka_odrezu = parseInt($("#delka_odrezu").val());
+  const delka_prorezu = parseInt($("#delka_prorezu").val());
+
+  const produktivni_delka_polotovaru = delka_polotovaru - delka_odpadu;
 
   let delky_vsech_trubek = [];
 
@@ -22,23 +26,23 @@ function spocitej_vysledky() {
     {
       cislo: 1,
       delka: 0,
-      odpad: delka_polotovaru,
+      odpad: produktivni_delka_polotovaru,
       rezy: []
     }
-  )
+  );
 
   while (trubky_od_nejdelsi_po_nejkratsi.length > 0) {
     let pridali_jsme_novou_trubku = false;
-    const trubka_se_kterou_pracujeme = vysledne_trubky_s_jejich_detaily[vysledne_trubky_s_jejich_detaily.length - 1]
-    const momentalni_delka_pridavane_trubky = trubka_se_kterou_pracujeme.delka
+    const trubka_se_kterou_pracujeme = vysledne_trubky_s_jejich_detaily[vysledne_trubky_s_jejich_detaily.length - 1];
+    const momentalni_delka_pridavane_trubky = trubka_se_kterou_pracujeme.delka;
     for (let i = 0; i < trubky_od_nejdelsi_po_nejkratsi.length; i++) {
       const delka_potencialni_nove_trubky = trubky_od_nejdelsi_po_nejkratsi[i];
-      const potencialni_delka_nove_trubky_s_odrezem = delka_potencialni_nove_trubky + delka_odrezu
-      const potencialni_nova_delka_pridavane_trubky = momentalni_delka_pridavane_trubky + potencialni_delka_nove_trubky_s_odrezem
-      if (potencialni_nova_delka_pridavane_trubky <= delka_polotovaru) {
+      const potencialni_delka_nove_trubky_s_odrezem = delka_potencialni_nove_trubky + delka_prorezu;
+      const potencialni_nova_delka_pridavane_trubky = momentalni_delka_pridavane_trubky + potencialni_delka_nove_trubky_s_odrezem;
+      if (potencialni_nova_delka_pridavane_trubky <= produktivni_delka_polotovaru) {
         pridali_jsme_novou_trubku = true;
         trubka_se_kterou_pracujeme.delka = potencialni_nova_delka_pridavane_trubky;
-        trubka_se_kterou_pracujeme.odpad = delka_polotovaru - potencialni_nova_delka_pridavane_trubky;
+        trubka_se_kterou_pracujeme.odpad = produktivni_delka_polotovaru - potencialni_nova_delka_pridavane_trubky;
         trubka_se_kterou_pracujeme.rezy.push(delka_potencialni_nove_trubky);
         trubky_od_nejdelsi_po_nejkratsi.splice(i, 1);
         break;
@@ -50,7 +54,7 @@ function spocitej_vysledky() {
       nova_trubka = {
         cislo: poradove_cislo_nove_trubky,
         delka: 0,
-        odpad: delka_polotovaru,
+        odpad: produktivni_delka_polotovaru,
         rezy: []
       };
       vysledne_trubky_s_jejich_detaily.push(nova_trubka);
@@ -97,41 +101,79 @@ function vymaz_trubku(event) {
   $("#" + moznost_na_vymaz).remove();
 }
 
+function only_unique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
 function zobraz_vysledky_do_tabulky(trubky_detaily) {
   for (let i = 0; i < trubky_detaily.length; i++) {
+    const rezy_pro_danou_trubku = trubky_detaily[i].rezy;
+    const kombinace = {};
+
+
+    let unikatni_trubky = rezy_pro_danou_trubku.filter(only_unique);
+    for (let unikatni_delka of unikatni_trubky) {
+      let pocet_trubek = 0;
+      for (delka of rezy_pro_danou_trubku) {
+        if (delka === unikatni_delka) {
+          pocet_trubek++;
+        }
+      }
+      kombinace[unikatni_delka] = pocet_trubek;
+    }
+    let kombinace_v_textu = [];
+    for (delka in kombinace) {
+      const novy_text = `${kombinace[delka]} x ${delka}`;
+      kombinace_v_textu.push(novy_text);
+    }
+    const cely_text_kombinaci = kombinace_v_textu.join(", ")
+    vysledne_kombinace.push(cely_text_kombinaci)
     $("#detaily_trubek").append(
       `
       <tr>
         <td> ${trubky_detaily[i].cislo} </td>
         <td> ${trubky_detaily[i].odpad} (+${$("#delka_odpadu").val()}) </td>
-        <td> ${trubky_detaily[i].rezy} </td>
+        <td> ${rezy_pro_danou_trubku} (${cely_text_kombinaci})</td>
       </tr>"
       `
     );
   }
 }
 
-function pocitej() {
-  odstran_predchozi_vysledky();
-  vysledky = spocitej_vysledky();
+function zapis_vysledky_do_zpravy(vysledky) {
   $("#pocet_trubek").html(vysledky.pocet_trubek);
+  $("#celkem_polotovaru").html(vysledky.pocet_trubek*parseInt($("#delka_polotovaru").val()));
   $("#celkovy_odpad").html(
       `${vysledky.celkovy_odpad} (+${vysledky.pocet_trubek}*${$("#delka_odpadu").val()})`
   );
+  let text_zpravy = ""
+  for ([index, kombinace] of vysledne_kombinace.entries()) {
+    const novy_text = `Profil ${index+1}: ${kombinace} <br>`;
+    text_zpravy += novy_text;
+  }
+  $("#zprava_detaily").html(text_zpravy);
+}
+
+function pocitej() {
+  vysledne_kombinace = [];
+  odstran_predchozi_vysledky();
+  vysledky = spocitej_vysledky();
   zobraz_vysledky_do_tabulky(vysledky.trubky_detaily);
+  zapis_vysledky_do_zpravy(vysledky);
 }
 
 function odstran_predchozi_vysledky() {
   $("#detaily_trubek").html(
     `
       <tr>
-        <th>Číslo trubky</th>
+        <th>Číslo profilu</th>
         <th>Zbytek [mm]</th>
         <th>Jednotlivé řezy [mm]</th>
       </tr>
     `
   );
   $("#pocet_trubek").html("");
+  $("#celkem_polotovaru").html("");
   $("#celkovy_odpad").html("");
 }
 
